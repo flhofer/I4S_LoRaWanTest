abde28f09ae8294832cef469d6e9de1ac7ee86ac
Florian Hofer <info@florianhofer.it>
2021-04-29 13:34:47 +0200

    MKRWan endswith for compat!

--- MKRWAN.h
+++ MKRWAN.h
@@ -1122,8 +1122,7 @@ private:
         if ((a > '0' && a != '=')  ||
         	(a != '\r' && a != ':' && a != ' '))
         	continue;				// continue receive until terminator
-        // Use endsWith instead of equals as if fails faster (common ERR root)
-        if (r1 && data.equals(r1)) {
+        if (r1 && data.endsWith(r1)) {
           index = 1;
           goto finish;
         } else if (r2 && data.endsWith(r2)) {
@@ -1147,7 +1146,7 @@ private:
         } else if (r8 && data.endsWith(r8)) {
           index = 8;
           goto finish;
-        } else if (data.equals("+RECV=")) {
+        } else if (data.endsWith("+RECV=")) {
           data = "";
           (void)stream.readStringUntil(',').toInt();
           length = stream.readStringUntil('\r').toInt();
